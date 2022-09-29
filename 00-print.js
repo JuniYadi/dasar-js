4214f9e5e3af02f8edaf095d28ddf48c8951a6a8
JuniYadi <juniyadi94@gmail.com>
2022-09-29 20:10:59 +0700

    Initial Commit

---
+++ 00-print.js
@@ -0,0 +1,4 @@
+console.log('Hai ... Selamat Belajar Javascript');
+console.log('Ini adalah baris kedua');
+console.log('Ini adalah baris ketiga');
+console.log('Ini adalah baris keempat');
---
+++ 03-pembagian.js
@@ -0,0 +1,6 @@
+const nilaiA = 10
+const nilaiB = 20
+
+const hasil = nilaiA / nilaiB
+
+console.log("Hasil dari perkalian nilaiA / nilaiB adalah " + hasil)
