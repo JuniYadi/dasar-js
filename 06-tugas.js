c2a965e1c0fd53ebb95fc621b11b689ffe7a776e
Juni Yadi <juni.yadi@pintar.co>
2022-10-20 20:13:22 +0700

    Added Tugas 6

---
+++ 06-tugas.js
@@ -0,0 +1,8 @@
+// buatkan tampilan berikut dengan looping dengan inputan dari user
+// 122333444455555666666
+
+// buatkan tampilan berikut dengan looping dengan inputan dari user
+// 666666123454444123221
+
+// buatkan tampilan berikut dengan looping dengan inputan dari user
+// 12345655555123433121
